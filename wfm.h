7d8e1aea30cb93a0794567d0cbe2411211f18b02
tenox7 <as@tenoware.com>
2017-12-14 00:44:54 -0800

    http basic/digest auth support

--- wfm.h
+++ wfm.h
@@ -1,4 +1,4 @@
-#define VERSION "1.2.3"
+#define VERSION "1.2.4"
 #define copyright "<!-- WFM Version " VERSION ", Mountain View, CA, " __DATE__ " [" __TIME__ "] -->\n" \
                   "<!-- Copyright (c) 1994-2017 by Antoni Sawicki -->\n"
 
