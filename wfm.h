ccd3aa06f871d1564ed3e95c339ab2768e9eed74
Antoni Sawicki <tenox@google.com>
2019-05-07 02:31:02 -0700

    copyright update

--- wfm.h
+++ wfm.h
@@ -1,7 +1,7 @@
 #define VERSION "1.4.1"
 #define COPYRIGHT "<!-- WFM Version " VERSION ", Mountain View, CA, " __DATE__ " [" __TIME__ "] -->\n" \
                   "<!-- Copyright (c) 1994-2018 by Antoni Sawicki -->\n" \
-                  "<!-- Copyright (c) 2019 by Google LLC -->\n"
+                  "<!-- Copyright (c) 2018-2019 by Google LLC -->\n"
 
 #define FONT_SIZE "12px"
 
