7e7d84b8ceaaae10819b568b1fca7eac90cdd038
Antoni Sawicki <tenox@google.com>
2022-03-14 03:09:30 -0700

    move todo to separate file

--- wfm.go
+++ wfm.go
@@ -1,34 +1,4 @@
 // Web File Manager
-//
-// TODO:
-// * CSS html/body height 100%
-// * checkboxes, multi file routines
-// * zip/unzip archives
-// * iso files recursive list
-// * du with xdev as a go routine
-// * two factor auth
-// * better handle cert chdir issue
-//   get and preload cert manually on start?
-//   hide acm cache dir?
-//   try different lib like lego?
-// * rate limiter with bad auth punishment (ip and user)
-// * git client https://github.com/go-git/go-git
-// * file locking https://github.com/gofrs/flock
-// * docker support (no chroot) - mount dir as / ?
-// * webdav server
-// * html as template
-// * archive files in main view / graphical/table form
-// * support for different filesystems, S3, SMB, archive files as io/fs
-// * separate icons for different file types like images
-// * editable and non editable documents by extension, also for git checkins
-// * thumbnail / icon view for pictures, cache thumbnails on server
-// * glob filter (*.*) in dir view
-// * FastCGI Interface
-// * Chroot and User in Systemd Unit
-// * Security Hardening in Systemd Unit
-// * Highlight newly uploaded file
-// * different icons for different file types
-// * Build summary in about dialog
 
 package main
 
