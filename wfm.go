5d8af7595c98042fe11d0b992551d1d17f592e77
Antoni Sawicki <as@tenoware.com>
2026-07-01 21:03:06 -0700

    bump ver

--- wfm.go
+++ wfm.go
@@ -37,7 +37,7 @@ type wfmPrefix struct {
 }
 
 var (
-	vers        = "2.3.1"
+	vers        = "2.4.0"
 	bindProto   = flag.String("proto", "tcp", "tcp, tcp4, tcp6, etc")
 	bindAddr    = flag.String("addr", ":8080", "Listen address, eg: :443")
 	bindExtra   = flag.String("addr_extra", "", "Extra non-TLS listener address, eg: :8081")
